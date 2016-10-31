ef58ebcc5428f9738e0ca48df9d00c039a92d616
Nate Hodge <nhodge253@gmail.com>
2016-10-30 23:25:47 -0700

    added map, styled elements

--- js/main.js
+++ js/main.js
@@ -10,36 +10,27 @@
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
 
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 
-var satLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
 
-var drawLayer = L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-});
-
-var mapLayers = {
-    "Satellite": satLayer,
-    "Map View": drawLayer,
-    "Open Street Maps": osm
-}
-
-var mymap = L.map('map-container').setView([46.852886, -121.760374], 13);
+var mymap = L.map('map-container').setView([46.852886, -121.760374], 7);
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
 // the map tiles layers:
-L.control.layers(mapLayers).addTo(map);
-satLayer.addTo(map);
 
-var marker = L.marker([46.852, -121.760]).addTo(map);
-marker.bindPopup("<b>Welcome to Mt. Rainier!</b><br>This peak is 4,392 feet high.");
+var osm = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
+
+L.tileLayer(osm).addTo(mymap);
+var marker = L.marker([46.852886, -121.760374]).addTo(mymap);
+marker.bindPopup("<b>Mt. Rainier</b>").openPopup();
 
 // TODO: Customize that Map to show markers with popups at no fewer than 3
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
+var marker2 = L.marker([46.1095143,-122.2135499]).addTo(mymap);
+marker2.bindPopup("<b>Ape Caves</b>").openPopup();
+
+var marker3 = L.marker([46.1574536,-119.4778029]).addTo(mymap);
+marker3.bindPopup("<b>Horse Heaven Hills Cemetary</b>").openPopup();
+
+var marker4 = L.marker([46.9770499,-119.3574428]).addTo(mymap);
+marker4.bindPopup("<b>Potholes State Park</b>").openPopup();
